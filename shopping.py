a9d25c00534cc6a2a9501ae1d48a69a950231b35
SeungJu <sjoh0704@daum.net>
2021-04-19 15:24:17 +0900

    implement tk module

--- shopping.py
+++ shopping.py
@@ -138,149 +138,42 @@ def compute_date(today, date):
     month = (today[0] - date[0]) * 12 + (today[1] - date[1])
     return month
 
-def __init__():
-    global main_category
-    today = set_today()
-    print(set_today())
-    main_category = int(input("해외: 1\t해외직구: 2\t직구: 3\n"))
-    sub_category = int(input("패션의류: 1, 가구/인테리어: 2, 생활/건강: 3, 스포츠/레저: 4, 식품: 5, 패션잡화: 6, 화장품/미용: 7, 디지털/가전: 8, 출산/육아: 9, 여가/생활편의:10\n"))
-    MONTH = int(input("몇 개월내로 검색하시겠습니까? :"))
-    URL_CNT = int(input("몇 개의 리스트를 가져오시겠습니까? :"))
-    driver = webdriver.Chrome(CHROME_LOCATION)
-
-
-
-    SCROLL_PAUSE_SEC = 0.01
-    # 스크롤 높이 가져옴
-    url_list = []
-    title_list = []
-    date_list = []
-    selected_URL_list = []
-    selected_title_list = []
-    selected_item = []
-
-    for i in range(1, 100):
-
-        try:
-            link = get_category(main_category, sub_category, i)
-
-            driver.get(link)
-        except Exception as e:
-            print(e)
-            break
-
-
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        while True:
-        # 끝까지 스크롤 다운
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # 1초 대기
-            time.sleep(SCROLL_PAUSE_SEC)
-
-        # 스크롤 다운 후 스크롤 높이 다시 가져옴
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-
-
-        html = driver.page_source
-        soup = BeautifulSoup(html, "html.parser")
-
-
-    # 타이틀
-        titles = soup.select('ul.list_basis div div div div div a[title]')
-
-        for title in titles:
-            text = title.get("title")
-            title_list.append(text)
-            # print(text)
-
-
-        # url
-
-        url_items = soup.select('ul.list_basis div div div div div a[href]')
-        # print(str(i) + "번째")
-        # print(url_items)
-
-        tmp = None
-        for url in url_items:
-            text = url.get("href")
-
-
-            if "https://cr" in text and text != tmp:
-                url_list.append(text)
-                tmp = text
-
-
-        # review
-        date_items = soup.select(".list_basis div span.basicList_etc__2uAYO")
-
-        for date in date_items:
-            text = date.get_text()
-            # print(tmp)
-            if "등록일" in text:
-                text = text.split(" ")[1]
-                date_list.append(text)
-                # print("안쪽")
-        for i, date in enumerate(date_list):
-            if compute_date(today, date) <= MONTH:
-                tmp = Item(title_list[i], url_list[i])
-                selected_item.append(tmp)
-
-        if len(selected_item) > URL_CNT:  # 가져오려는 리스트수
-            driver.close()
-            selected_item = selected_item[:URL_CNT]
-            break
-    for i in range(URL_CNT):
-        print(str(i + 1) + ". " + selected_item[i].title +"\n URL: ", selected_item[i].url + "\n")
-
-
-    print()
-
-    make_file(selected_item)
-
-
-    # print("< 상품 필터링하기 >")
-    # while(True):
-    #     tmp_list = []
-    #     x = input("빼고 싶은 상품 입력(-1입력시 종료): ")
-    #     y = 0
-    #     if x == "-1":
-    #         print("종료합니다")
-    #
-    #         break
-    #
-    #     i = 0
-    #     for i in range(len(selected_item)):
-    #         if(x not in selected_item[i].title):
-    #             tmp_list.append(selected_item[i])
-
-    #     if y == len(selected_item):
-    #         print("상품 없음")
-    #         break
-    #     selected_item = tmp_list
-    #
-    # print()
-    # print("< 필터링된 상품 목록 >")
-    # for i in range(len(selected_item)):
-    #     print(str(i + 1) + ". " + selected_item[i].title +"\n URL: ", selected_item[i].url + "\n")
-
-# __init__()
 def setWay():
     way_category = way_var.get()
     return way_category
+
 def setWhat():
     what_category = what_var.get()
     return what_category
+
 def setMonth():
     month = Entry.get(month_entry)
     print(month)
     return month
 
-WAY = ["해외", "해외직구","직구"]
+def setListCnt():
+    list_cnt = Entry.get(list_entry)
+    print(list_cnt)
+    return list_cnt
+
+def finalClick():
+    global main_category
+    global sub_category
+    global MONTH
+    global URL_CNT
+    main_category = setWay()
+    sub_category = setWhat()
+    MONTH = int(setMonth())
+    URL_CNT = int(setListCnt())
+
+today = set_today()
+print(set_today())
+
+global main_category
+global sub_category
+global MONTH
+global URL_CNT
+WAY = ["해외", "해외직구", "직구"]
 WHAT = ["패션의류", "가구/인테리어", "생활/건강", "스포츠/레저", "식품", "패션잡화", "화장품/미용", "디지털/가전", "출산/육아", "여가/생활편의"]
 window = Tk()
 window.geometry("400x400")
@@ -290,36 +183,164 @@ way_btn = []
 what_btn = []
 way_var = IntVar()
 for i, way_title in enumerate(WAY):
-    way_btn.append(Radiobutton(window, text = way_title, variable = way_var, value=i+1, command=setWay))
+    way_btn.append(Radiobutton(window, text=way_title, variable=way_var, value=i + 1, command=setWay))
 x = 20
 y = 10
 for i in range(len(WAY)):
-    way_btn[i].place(x=x,  y=y)
-    x+= 70
-
+    way_btn[i].place(x=x, y=y)
+    x += 70
 
 what_var = IntVar()
 y += 50
 for i, what_title in enumerate(WHAT):
-    what_btn.append(Radiobutton(window, text=what_title, variable=what_var, value=i+1, command=setWhat))
+    what_btn.append(Radiobutton(window, text=what_title, variable=what_var, value=i + 1, command=setWhat))
 x = 20
 for i in range(len(WHAT)):
     what_btn[i].place(x=x, y=y)
-    y+=30
+    y += 30
 x = 200
-y = 200
-mon_var = IntVar()
-month_entry = Entry(window, width=20)
+y = 50
+
+month_text = Label(window, text="몇개월이내로 가져오시겠습니까?")
+month_entry = Entry(window, width=10)
+month_text.place(x=x, y=y)
+y += 20
 month_entry.place(x=x, y=y)
+y += 40
+list_cnt = Label(window, text="몇개의 리스트를 가져오시겠습니까?")
+list_entry = Entry(window, width=10)
+list_cnt.place(x=x, y=y)
+y += 20
+list_entry.place(x=x, y=y)
 
-
-
-
-label = Button(window, text = "click", command=setMonth)
+label = Button(window, text="크롤링 시작", command=finalClick)
 label.place(x=300, y=300)
 window.mainloop()
+# main_category = int(input("해외: 1\t해외직구: 2\t직구: 3\n"))
+# sub_category = int(input("패션의류: 1, 가구/인테리어: 2, 생활/건강: 3, 스포츠/레저: 4, 식품: 5, 패션잡화: 6, 화장품/미용: 7, 디지털/가전: 8, 출산/육아: 9, 여가/생활편의:10\n"))
+# MONTH = int(input("몇 개월내로 검색하시겠습니까? :"))
+# URL_CNT = int(input("몇 개의 리스트를 가져오시겠습니까? :"))
+driver = webdriver.Chrome(CHROME_LOCATION)
 
 
+
+SCROLL_PAUSE_SEC = 0.01
+# 스크롤 높이 가져옴
+url_list = []
+title_list = []
+date_list = []
+selected_URL_list = []
+selected_title_list = []
+selected_item = []
+
+for i in range(1, 100):
+
+    try:
+        link = get_category(main_category, sub_category, i)
+
+        driver.get(link)
+    except Exception as e:
+        print(e)
+        break
+
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+    # 끝까지 스크롤 다운
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # 1초 대기
+        time.sleep(SCROLL_PAUSE_SEC)
+
+    # 스크롤 다운 후 스크롤 높이 다시 가져옴
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+
+# 타이틀
+    titles = soup.select('ul.list_basis div div div div div a[title]')
+
+    for title in titles:
+        text = title.get("title")
+        title_list.append(text)
+        # print(text)
+
+
+    # url
+
+    url_items = soup.select('ul.list_basis div div div div div a[href]')
+    # print(str(i) + "번째")
+    # print(url_items)
+
+    tmp = None
+    for url in url_items:
+        text = url.get("href")
+
+
+        if "https://cr" in text and text != tmp:
+            url_list.append(text)
+            tmp = text
+
+
+    # review
+    date_items = soup.select(".list_basis div span.basicList_etc__2uAYO")
+
+    for date in date_items:
+        text = date.get_text()
+        # print(tmp)
+        if "등록일" in text:
+            text = text.split(" ")[1]
+            date_list.append(text)
+            # print("안쪽")
+    for i, date in enumerate(date_list):
+        if compute_date(today, date) <= MONTH:
+            tmp = Item(title_list[i], url_list[i])
+            selected_item.append(tmp)
+
+    if len(selected_item) > URL_CNT:  # 가져오려는 리스트수
+        driver.close()
+        selected_item = selected_item[:URL_CNT]
+        break
+for i in range(URL_CNT):
+    print(str(i + 1) + ". " + selected_item[i].title +"\n URL: ", selected_item[i].url + "\n")
+
+
+print()
+
+make_file(selected_item)
+
+
+# print("< 상품 필터링하기 >")
+# while(True):
+#     tmp_list = []
+#     x = input("빼고 싶은 상품 입력(-1입력시 종료): ")
+#     y = 0
+#     if x == "-1":
+#         print("종료합니다")
+#
+#         break
+#
+#     i = 0
+#     for i in range(len(selected_item)):
+#         if(x not in selected_item[i].title):
+#             tmp_list.append(selected_item[i])
+
+#     if y == len(selected_item):
+#         print("상품 없음")
+#         break
+#     selected_item = tmp_list
+#
+# print()
+# print("< 필터링된 상품 목록 >")
+# for i in range(len(selected_item)):
+#     print(str(i + 1) + ". " + selected_item[i].title +"\n URL: ", selected_item[i].url + "\n")
 
 
 
